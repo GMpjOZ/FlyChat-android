e29a9a06b8e71373ca5d0528a3b7a9a3260d4095
GMpj <good_mpj@163.com>
2015-04-24 21:54:40 +0800

    初步完成地图定位

---
+++ app/src/main/java/flaychat/cn/flychat/map/Local.java
@@ -0,0 +1,9 @@
+package flaychat.cn.flychat.map;
+
+/**
+ * Created by MPJ on 15/4/24.
+ */
+public class Local {
+
+
+}
--- app/src/main/java/flaychat/cn/flychat/map/MapActivity.java
+++ app/src/main/java/flaychat/cn/flychat/map/MapActivity.java
@@ -1,18 +1,29 @@
 package flaychat.cn.flychat.map;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BaiduMapOptions;
 import com.baidu.mapapi.map.BitmapDescriptor;
 import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
 import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.MyLocationData;
 import com.baidu.mapapi.map.OverlayOptions;
 import com.baidu.mapapi.model.LatLng;
 
@@ -20,35 +31,107 @@ import flaychat.cn.flychat.R;
 
 public class MapActivity extends ActionBarActivity {
     MapView mMapView = null;
-//    public LocationClient mLocationClient = null;
-//    public BDLocationListener myListener = new MyLocationListener();
+    BaiduMap mBaiduMap;
+    public LocationClient mLocationClient = null;
+    boolean isFirstLoc = true;// 是否首次定位
+    double latitude=39.26;
+    double longitude=115.25;
+    public BDLocationListener myListener = new BDLocationListener() {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+
+            if (location == null || mMapView == null)
+                return;
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                            // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(100).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                LatLng ll = new LatLng(location.getLatitude(),
+                        location.getLongitude());
+                MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(ll);
+                mBaiduMap.animateMapStatus(u);
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        SDKInitializer.initialize(getApplicationContext());
-//         mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
-//        mLocationClient.registerLocationListener( myListener );    //注册监听函数
+        SDKInitializer.initialize(getApplicationContext());
+        mLocationClient = new LocationClient(getApplicationContext());     //声明LocationClient类
+        mLocationClient.registerLocationListener( myListener );    //注册监听函数
 
         setContentView(R.layout.activity_map);
         mMapView = (MapView) findViewById(R.id.bmapView);
-        BaiduMap mBaiduMap = mMapView.getMap();
-//普通地图
+        mBaiduMap = mMapView.getMap();
         mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+        mBaiduMap.setMyLocationEnabled(true);
+        mBaiduMap.setMaxAndMinZoomLevel(17,17);
 
-        //定义Maker坐标点
-        LatLng point = new LatLng(39.963175, 116.400244);
-//构建Marker图标
-        BitmapDescriptor bitmap = BitmapDescriptorFactory
-                .fromResource(R.drawable.icon_marki);
-//构建MarkerOption，用于在地图上添加Marker
-        OverlayOptions option = new MarkerOptions()
-                .position(point)
-                .icon(bitmap);
-//在地图上添加Marker，并显示
-        mBaiduMap.addOverlay(option);
+        Log.w("最小缩放",mBaiduMap.getMinZoomLevel()+"");
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true);// 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(10000);
+        mLocationClient.setLocOption(option);
+        mLocationClient.start();
+
+
+
     }
-
-
+//
+//    //创建handler
+//    Handler updateBarHandler = new Handler(){
+//        @Override
+//        public void handleMessage(Message msg) {
+//
+//            Log.w("纬度1",latitude+"");
+//            Log.w("经度1",longitude+"");
+//            LatLng point = new LatLng(latitude, longitude);
+////            MapView nMapView=new MapView(getApplicationContext(),
+////                    new BaiduMapOptions().mapStatus(new MapStatus.Builder()
+////                            .target(point).build()));
+////            setContentView(nMapView);
+//            mBaiduMap.setMyLocationEnabled(true);
+//
+//            MyLocationData myLocationData=new MyLocationData.Builder().latitude(latitude).longitude(longitude).build();
+//
+//            mBaiduMap.setMyLocationData(myLocationData);
+//            //构建Marker图标
+//            BitmapDescriptor bitmap = BitmapDescriptorFactory
+//                    .fromResource(R.drawable.icon_marki);
+////构建MarkerOption，用于在地图上添加Marker
+//            OverlayOptions option = new MarkerOptions()
+//                    .position(point)
+//                    .icon(bitmap);
+////             mBaiduMap = nMapView.getMap();
+////在地图上添加Marker，并显示
+//            mBaiduMap.addOverlay(option);
+//                    }
+//
+//
+//    };
+//    //使用线程进程数据处理
+//    class MyThread implements Runnable {
+//        public void run() {
+//
+//
+//                mLocationClient.start();
+//            mLocationClient.requestLocation();
+//            Log.w("location",mLocationClient.requestLocation()+"");
+////                while (true){
+////                    if(mLocationClient.requestLocation())
+////                        break;
+////                }
+//
+//            // 向Handler发送消息,更新UI
+//
+//
+//        }
+//    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
