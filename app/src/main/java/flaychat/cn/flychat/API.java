914aa1218379829c905322c737aa8464f04e411a
GMpj <good_mpj@163.com>
2015-04-25 20:11:30 +0800

    初步完成地图定位

--- app/src/main/java/flaychat/cn/flychat/API.java
+++ app/src/main/java/flaychat/cn/flychat/API.java
@@ -5,9 +5,11 @@ package flaychat.cn.flychat;
  */
 public class API {
 
-    public static String URL="http://192.168.1.100:8080/FlayChat/";
+    public static String URL="http://192.168.1.126:8080/FlayChat/";
 
     public static String WelcomeURL=URL+"NewFile.html";
 
-    public static  String GetUser=URL+"";
+    public static  String GetUser=URL+"GetUserServlet";
+
+    public static  String PostLocation=URL+"PostLocationServlet";
 }
